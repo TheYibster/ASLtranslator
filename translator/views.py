9260364d7a634efb98c532686480d0b9e89fa5fd
YiboLiang <yiboliang02@gmail.com>
2023-01-24 14:06:34 -0800

    needed to handle try/except

--- translator/views.py
+++ translator/views.py
@@ -40,5 +40,5 @@ def livecam(request):
     try:
         cam = VideoCamera()
         return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        pass
+    except:  
+        return render(request, "translator/error.html", content={}) 
